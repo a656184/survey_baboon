5a4c272c8c936cf0aa7c5048615cd102a01da332
dbc-warrior <apprentice@devbootcamp.com>
2013-08-16 14:53:40 -0700

    add better seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,21 @@
 require 'faker'
 
+Survey.destroy_all
+Question.destroy_all
+Choice.destroy_all
+
 10.times do 
   title = Faker::Company.catch_phrase
   description = Faker::Company.bs
-  Survey.create(title: title, description: description)
+  survey = Survey.create(title: title, description: description)
+  10.times do
+    content = Faker::Company.bs + '?'
+    question = Question.create(content: content)
+    survey.questions << question
+    4.times do 
+      content = Faker::Company.name
+      choice = Choice.create(content: content)
+      question.choices << choice
+    end
+  end
 end
