66c5eae068968b80828d2eb3f8cd0038dfa216ed
dbc-warrior <apprentice@devbootcamp.com>
2013-08-16 14:46:41 -0700

    add basic survey show page

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -4,3 +4,8 @@ get '/protected/surveys' do
   @surveys = Survey.all
   haml :surveys
 end
+
+get '/protected/surveys/:survey_id' do
+  @survey = Survey.find_by_id(params[:survey_id])
+  haml :survey_show
+end
