2c4ff811cc9c596a473b294bae38cdfe87ac7c28
Anirudh Dhullipalla <rudysback@gmail.com>
2013-08-17 17:36:07 -0700

    make js prevent submit if title or description invalid, character count in view

--- public/js/application.js
+++ public/js/application.js
@@ -4,8 +4,19 @@ $(document).ready(function() {
   // when we try to bind to them
 
   // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  var titleValid = false
+  var descriptionValid = false
 
 
+  $('#title').on('keyup', function(){
+    titleValid = updateCounter($(this), 25, 0);
+    updateSubmit();
+  });
+
+  $('#description').on('keyup', function(){
+    descriptionValid = updateCounter($(this), 250, 10)
+    updateSubmit();
+  });
 
   $(".add_question").on('click', function(e){
     e.preventDefault();
@@ -34,7 +45,40 @@ $(document).ready(function() {
   });
 
 
+  function updateCounter(selector, max, min){
+    var charCount = selector.val().length;
+    var charCountLeft = max-charCount
+    var $text_counter = selector.next(".text_counter")
+    $text_counter.text(charCountLeft);
+
+    if (charCountLeft < 0 || charCount < min) {
+      $text_counter.css('color', 'red')
+    }
+    else {
+      $text_counter.css('color', 'black')
+    }
+
+    if (charCount > min && charCount < max) {
+      return true
+    }
+    else {
+      return false
+    }
+  };
+
+
+  function updateSubmit(){
+    console.log(titleValid)
+    console.log(descriptionValid)
+    if (titleValid && descriptionValid) {
+      $('#submit').removeAttr("disabled");
+    }
+    else {
+      $('#submit').attr("disabled", "disabled");
+    }
+  }
 });
+
 
 
 
