f2d73fc353f2cd0648dfd19901fa00bd9e74457a
Matthew Weitzel <mweitzel@gmail.com>
2013-08-20 10:47:49 -0700

    code review

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -3,6 +3,14 @@ before do
   @user_id = session[:user_id]
 end
 
+
+# so, this is effective, but makes the url gross and
+# doesn't allow for limited viewing, like if you 
+# wanted to share results with non-users
+
+# there are ways to do before filters for specific controllers
+# and present them with a @current_user object
+
 before '/protected/*' do
   unless logged_in?
     redirect to '/sessions/new'
--- app/controllers/survey_participations.rb
+++ app/controllers/survey_participations.rb
@@ -1,5 +1,14 @@
 # if survey not taken -> show a survey form for answering
 # else -> redirect to  result view
+
+
+# I like that you do this, but if you are giving very different
+# logic depending on state, maybe give it a different linke
+#
+# I like what this does, but there is no way to finish a survey
+# once I leave the page, even it 1/20 questions answered
+# if it was a different route, you could build the link list better
+
 get '/protected/surveys/:survey_id/participation' do
   @survey = Survey.find_by_id(params[:survey_id])
 
@@ -16,16 +25,22 @@ end
 # submit answers for a survey
 post '/protected/surveys/:survey_id' do
     
-    survey = Survey.find_by_id(params[:survey_id])
+	#	this is a 15 line code block that could absooutely be 
+	# extracted into the SurveyParticipation class.
+	# 
+	# where t stands
 
-    survey_participation = SurveyParticipation.new
-  
-    survey_participation.user = current_user
-    survey_participation.survey = survey
-    survey_participation.completion_status = 'complete'
-    survey_participation.save
-  
-  params[:survey].each do |question_id, choice_id|
+	# indentation is important!
+	survey = Survey.find_by_id(params[:survey_id])
+
+	survey_participation = SurveyParticipation.new
+
+	survey_participation.user = current_user
+	survey_participation.survey = survey
+	survey_participation.completion_status = 'complete'
+	survey_participation.save
+
+  prams[:survey].each do |question_id, choice_id|
     
     answer = Answer.new
     answer.survey_participation = survey_participation
